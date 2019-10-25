443bac5e2ca1bddf2fdb1c9a72b164c158688afe
Ascander Dost <dostinthemachine@gmail.com>
2019-10-24 21:22:20 -0700

    Change fullscreen binding to 'f'

--- init.el
+++ init.el
@@ -590,7 +590,7 @@ and ':underline' the same value."
   (other-window 1))
 
 (general-t
-  "m" #'toggle-frame-fullscreen
+  "f" #'toggle-frame-fullscreen
   "h" #'windmove-left
   "j" #'windmove-down
   "k" #'windmove-up
