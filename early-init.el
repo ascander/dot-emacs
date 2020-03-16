d90ab758b1d760e67926775a3d892c8cbf6fb654
Ascander Dost <ascander@users.noreply.github.com>
2020-03-16 14:53:23 -0700

    OPPS

--- early-init.el
+++ early-init.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2020  Ascander Dost
 
-;; Author: Ascander Dost <adost@adost-ltm2.internal.salesforce.com>
-;; Keywords: convenience, convenience
+;; Author: Ascander Dost
+;; Keywords: convenience
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
